476893884a37ab84af3a184f689dee7aec867f48
HornetRatHead <53807789+HornetRatHead@users.noreply.github.com>
2024-03-23 23:57:27 +0200

    Bash file

---
+++ Tsygankov_CS_HW_2.1_bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+websites=("https://google.com" "https://facebook.com" "https://twitter.com")
+logfile="website_status.log"
+for website in "${websites[@]}"
+do
+  response=$(curl -s -o /dev/null -w "%{http_code}" "$website")
+  if [ "$response" -eq 200 ]; then
+    status="UP"
+  else
+    status="DOWN"
+  fi
+  echo "[${website}] is ${status}" >> "$logfile"
+done
+echo "Результати записані у файл $logfile"
